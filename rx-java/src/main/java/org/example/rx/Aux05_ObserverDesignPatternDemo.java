145dc90c250af1de26aa22bd11e58025b10ff126
ilyashabelnik <shabelnik_ilya@mail.ru>
2025-08-06 17:34:48 +0300

    - другой вариант асинхронного выполнения задачи

--- rx-java/src/main/java/org/example/rx/Aux05_ObserverDesignPatternDemo.java
+++ rx-java/src/main/java/org/example/rx/Aux05_ObserverDesignPatternDemo.java
@@ -9,6 +9,7 @@ package org.example.rx;
 
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
 
 public class Aux05_ObserverDesignPatternDemo {
     static interface Tweetable{
@@ -32,10 +33,15 @@ public class Aux05_ObserverDesignPatternDemo {
 
         @Override
         public void tweet() {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("Second fan: I was tweeted on: " + Thread.currentThread().getName());
         }
     }
-    static class RockStar{
+    static class SyncRockStar {
 
         private final List<Tweetable> fans = new ArrayList<>();
 
@@ -49,8 +55,26 @@ public class Aux05_ObserverDesignPatternDemo {
         }
     }
 
+    static class AsyncRockStar {
+
+        private final List<Tweetable> fans = new ArrayList<>();
+
+
+        public void add(Tweetable tweetable) {
+            fans.add(tweetable);
+        }
+
+        public void sendTweet() {
+            var tasks = fans.stream()
+                    .map(f -> CompletableFuture.runAsync(f::tweet, Executors.newCachedThreadPool()))
+                    .toList();
+
+            tasks.forEach(CompletableFuture::join);
+        }
+    }
+
     public static void main(String[] args) {
-        RockStar rockStar = new RockStar();
+        SyncRockStar rockStar = new SyncRockStar();
         rockStar.add(new Fan());
         rockStar.add(new AnotherFan());
 
@@ -58,10 +82,23 @@ public class Aux05_ObserverDesignPatternDemo {
         System.out.println("запуск синхронный рассылки твитов");
         System.out.println("________________________________");
 
+        var start = System.currentTimeMillis();
         CompletableFuture<Void> asyncTweet = CompletableFuture.runAsync(rockStar::sendTweet); // асинхронная
         asyncTweet.thenRun(() -> System.out.println("Асинхронное выполнение задачи окончено"));
         System.out.println("между результатом асинхронной задачи");
         asyncTweet.join();
+        var end = System.currentTimeMillis() - start;
+        System.out.println("первый асинхронный вариант: " + end);
+
+        System.out.println("________________");
+        AsyncRockStar asyncRockStar = new AsyncRockStar();
+        asyncRockStar.add(new Fan());
+        asyncRockStar.add(new AnotherFan());
+
+        var asyncStart = System.currentTimeMillis();
+        asyncRockStar.sendTweet();
+        var asyncEnd = System.currentTimeMillis() - asyncStart;
+        System.out.println("Второй асинхронный вариант: " + asyncEnd);
     }
 }
 
